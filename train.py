878137ff22e42b1f6f7e01fa4fe4eae8af79c577
Mat Kelcey <matthew.kelcey@gmail.com>
2021-02-25 10:42:45 +1100

    overfitting loop ok

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
 import jax
-from jax import jit
+from jax import jit, value_and_grad
 import jax.numpy as jnp
 import models
 import haiku as hk
 import data as d
+import optax
 
 
 class Options:
     seed = 123
+    learning_rate = 1e-4
+    batch_size = 32
 
 
 opts = Options()
@@ -20,6 +23,9 @@ pod_rng, init_key = jax.random.split(pod_rng)
 representative_input = jnp.zeros((1, 64, 64, 13))
 params = model.init(init_key, representative_input)
 
+opt = optax.adam(opts.learning_rate)
+opt_state = opt.init(params)
+
 
 @jit
 def predict(params, x):
@@ -27,6 +33,30 @@ def predict(params, x):
     return jnp.argmax(logits, axis=-1)
 
 
-for x, labels in d.dataset(split='sample', batch_size=16):
-    print(predict(params, x))
-    break
+def softmax_cross_entropy(logits, labels):
+    one_hot = jax.nn.one_hot(labels, logits.shape[-1])
+    return -jnp.sum(jax.nn.log_softmax(logits) * one_hot, axis=-1)
+
+
+def mean_cross_entropy(params, x, y_true):
+    logits = model.apply(params, x)
+    return jnp.mean(softmax_cross_entropy(logits, y_true))
+
+
+@jit
+def update(params, opt_state, x, y_true):
+    loss, grads = value_and_grad(mean_cross_entropy)(params, x, y_true)
+    updates, opt_state = opt.update(grads, opt_state, params)
+    params = optax.apply_updates(params, updates)
+    return params, opt_state, loss.mean()
+
+
+for i, (x, y_true) in enumerate(d.dataset(split='train',
+                                          batch_size=opts.batch_size)):
+    params, opt_state, loss = update(params, opt_state, x, y_true)
+    if i % 100 == 0:
+        y_pred = predict(params, x)
+        num_correct = jnp.sum(jnp.equal(y_pred, y_true))
+        print(i, loss, num_correct, y_true, y_pred)
+    if i > 1000:
+        break
